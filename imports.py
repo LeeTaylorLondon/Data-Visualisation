99feff338f05308ee796a2e9c60146829864c53c
b9021147 <b9021147@ncl.ac.uk>
2022-11-19 19:10:54 +0000

    Replaced main with Collage

--- imports.py
+++ imports.py
@@ -1,3 +1,4 @@
+# Author: Lee Taylor, ST Number: 190211479
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 # Author: Lee Taylor, ST Number: 190211479
-from imports import *
-from part_a import part_a
-from part_b import part_b
-from part_c import part_c
-from part_d import part_d
+from collage import collage
 
-''' Create a 16:9 4 panel plot '''
-fig, (row1, row2) = plt.subplots(2, 3, figsize=(19.2, 10.8),
-                                 width_ratios=[1, 1, 1])
-
-# Bar charts
-part_a(row1[0], row1[1], False)
-
-# Scatter plots
-part_b(row2[0], row2[1], False)
-
-# Time series
-part_c(row2[2], False)
-
-# Text Description
-part_d()
-
-fig.show()
-plt.savefig('Images/final')
 
 if __name__ == '__main__':
-    # Create 4 panel plot as figmain
-    # Save figmain
-    # [Optional] prompt user to open figmain
-    pass
+    collage(show=True, save=False)
